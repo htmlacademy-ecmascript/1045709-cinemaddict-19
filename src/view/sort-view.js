f3a981488e84bddbd36f180d01db73dcd1cf341d
Samefral <galchun174@mail.ru>
2023-01-27 18:16:10 +0500

    mini refactoring sort-view

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -3,21 +3,13 @@ import { SortType } from '../consts.js';
 
 const createSortTemplate = (currentSortType) => (
   `<ul class="sort">
-      <li>
-        <a href="#" class="sort__button ${currentSortType === SortType.DEFAULT ? 'sort__button--active' : ''}" data-sort-type="${SortType.DEFAULT}">
-          Sort by default
-        </a>
-      </li>
-      <li>
-        <a href="#" class="sort__button ${currentSortType === SortType.DATE ? 'sort__button--active' : ''}" data-sort-type="${SortType.DATE}">
-          Sort by date
-        </a>
-      </li>
-      <li>
-        <a href="#" class="sort__button ${currentSortType === SortType.RATING ? 'sort__button--active' : ''}" data-sort-type="${SortType.RATING}">
-          Sort by rating
-        </a>
-      </li>
+      ${Object.values(SortType).map((sortType) => `
+        <li>
+          <a href="#" class="sort__button ${currentSortType === sortType ? 'sort__button--active' : ''}" data-sort-type="${sortType}">
+            Sort by ${sortType}
+          </a>
+        </li>
+    `).join('')}
    </ul>`
 );
 
