ffc9585cc1d55dd8e699dc8729b2369eac948bac
Samefral <galchun174@mail.ru>
2023-02-08 04:23:28 +0500

    fixed incorrect time template

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,12 @@ import dayjs from 'dayjs';
 
 const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
 
+const getTimeFromMins = (minutes) => `${Math.trunc(minutes / 60)}h ${minutes % 60}m`;
+
 const humanizeDate = (date, format) => date ? dayjs(date).format(format) : '';
 
 const isEscPressed = (evt) => evt.code === 'Escape';
 
 const isCtrlPlusEnterPressed = (evt) => evt.ctrlKey && evt.code === 'Enter';
 
-export { getRandomArrayElement, humanizeDate, isEscPressed, isCtrlPlusEnterPressed };
+export { getRandomArrayElement, getTimeFromMins, humanizeDate, isEscPressed, isCtrlPlusEnterPressed };
