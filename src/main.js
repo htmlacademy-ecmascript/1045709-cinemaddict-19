adde5c3ecc4737dfb57ccbe7d7f0c85c393789b5
Samefral <galchun174@mail.ru>
2023-02-08 09:28:28 +0500

    quantity statistics implemented

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import QuantityStatisticsView from './view/quantity-statistics-view.js';
-import { render } from './framework/render.js';
 import FilmsPresenter from './presenter/films-presenter.js';
 import FilterModel from './model/filter-model.js';
 import FilmsModel from './model/films-model.js';
@@ -10,9 +8,7 @@ import CommentsApiService from './service/api/comments-api-service.js';
 const AUTHORIZATION = 'Basic tKF54Tgh3jd';
 const END_POINT = 'https://19.ecmascript.pages.academy/cinemaddict';
 
-const siteHeader = document.querySelector('.header');
 const siteMain = document.querySelector('.main');
-const siteFooter = document.querySelector('.footer__statistics');
 
 const filmsModel = new FilmsModel({
   filmsApiService: new FilmsApiService(END_POINT, AUTHORIZATION)
@@ -25,16 +21,12 @@ const commentsModel = new CommentsModel({
 
 const filterModel = new FilterModel();
 
-const filmsQuantity = filmsModel.films.length.toLocaleString();
 const filmsPresenter = new FilmsPresenter({
   filmsContainer: siteMain,
-  userRankContainer: siteHeader,
   filmsModel,
   commentsModel,
   filterModel
 });
-
-render(new QuantityStatisticsView({filmsQuantity}), siteFooter);
 
 filmsPresenter.init();
 filmsModel.init();
